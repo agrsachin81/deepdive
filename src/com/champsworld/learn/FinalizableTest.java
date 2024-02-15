86b35775bc3ed957ee7370402ea4bd04e35f7ffb
agrsachin81 <agrsachin81@gmail.com>
2024-02-15 19:44:54 +0530

    renamed class

--- src/com/champsworld/learn/FinalizableTest.java
+++ src/com/champsworld/learn/FinalizableTest.java
@@ -3,18 +3,18 @@ package com.champsworld.learn;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class finalizableTest {
+public class FinalizableTest {
 
         private static final AtomicInteger idGen = new AtomicInteger(0);
         private final int id;
         private final Map<?,?> map;
-        public finalizableTest(Map<?, ?> testMap){
+        public FinalizableTest(Map<?, ?> testMap){
             id = idGen.getAndIncrement();
             System.out.println("CREATED finalize TEST "+ id +" "+System.identityHashCode(this) + " --map = "+System.identityHashCode(testMap));
             this.map = testMap;
         }
 
-        public finalizableTest(){
+        public FinalizableTest(){
             this(null);
         }
 
--- src/com/champsworld/learn/WeakReferenceTests.java
+++ src/com/champsworld/learn/WeakReferenceTests.java
@@ -18,62 +18,62 @@ public class WeakReferenceTests {
     }
 
     private static void testWithSet() {
-        Set<WeakReference<finalizableTest>> weakSet = new HashSet<>();
-        Set<finalizableTest> strongSet = new HashSet<>();
+        Set<WeakReference<FinalizableTest>> weakSet = new HashSet<>();
+        Set<FinalizableTest> strongSet = new HashSet<>();
 
         System.out.println("------START ADDING WEAK SET");
-        weakSet.add(new WeakReference<>(new finalizableTest()));
-        weakSet.add(new WeakReference<>(new finalizableTest()));
+        weakSet.add(new WeakReference<>(new FinalizableTest()));
+        weakSet.add(new WeakReference<>(new FinalizableTest()));
         System.out.println("------END ADDING WEAK SET");
 
         System.out.println("------START ADDING STRONG SET");
-        strongSet.add(new finalizableTest());
-        strongSet.add(new finalizableTest());
+        strongSet.add(new FinalizableTest());
+        strongSet.add(new FinalizableTest());
         System.out.println("------END ADDING STRONG SET");
         Runtime.getRuntime().gc();
 
-        for (WeakReference<finalizableTest> obj: weakSet) {
+        for (WeakReference<FinalizableTest> obj: weakSet) {
             System.out.println("WEAKSET value found "+obj.get());
         }
         System.out.println("SIZE OF WEAK SET "+ weakSet.size());
         System.out.println("SIZE OF STRONG SET "+ strongSet.size());
 
-        for (finalizableTest obj: strongSet) {
+        for (FinalizableTest obj: strongSet) {
             System.out.println("STRONG SET value found "+obj.toString());
         }
     }
 
     private static void testWithMap() {
 
-        WeakHashMap<finalizableTest, Integer> weakHashMap= new WeakHashMap<>();
-        Map<finalizableTest, Integer> strongMap = new HashMap<>();
+        WeakHashMap<FinalizableTest, Integer> weakHashMap= new WeakHashMap<>();
+        Map<FinalizableTest, Integer> strongMap = new HashMap<>();
 
         System.out.println("------START ADDING Weak MAP");
-        weakHashMap.put(new finalizableTest(weakHashMap), 1);
-        weakHashMap.put(new finalizableTest(weakHashMap), 2);
+        weakHashMap.put(new FinalizableTest(weakHashMap), 1);
+        weakHashMap.put(new FinalizableTest(weakHashMap), 2);
         System.out.println("------END ADDING Weak MAP");
 
         System.out.println("------START ADDING STRONG MAP");
-        strongMap.put(new finalizableTest(strongMap), 3);
-        strongMap.put(new finalizableTest(strongMap), 4);
+        strongMap.put(new FinalizableTest(strongMap), 3);
+        strongMap.put(new FinalizableTest(strongMap), 4);
         System.out.println("------END ADDING STRONG MAP");
         Runtime.getRuntime().gc();
 
-        for (finalizableTest obj: weakHashMap.keySet()) {
+        for (FinalizableTest obj: weakHashMap.keySet()) {
             System.out.println("WEAKHASHAMP value found "+obj);
         }
         System.out.println("SIZE OF WEAK MAP "+ weakHashMap.size());
         System.out.println("SIZE OF STRONG MAP "+ strongMap.size());
 
-        for (finalizableTest obj: strongMap.keySet()) {
+        for (FinalizableTest obj: strongMap.keySet()) {
             System.out.println("STRONG MAP value found "+obj.toString());
         }
     }
 
     private static void testWithMapThreadAsKey() {
 
-        WeakHashMap<Thread, finalizableTest> weakHashMap= new WeakHashMap<>();
-        Map<Thread, finalizableTest> strongMap = new HashMap<>();
+        WeakHashMap<Thread, FinalizableTest> weakHashMap= new WeakHashMap<>();
+        Map<Thread, FinalizableTest> strongMap = new HashMap<>();
         
         System.out.println("------START ADDING Weak MAP THREAD");
         Set<Thread> threads = new HashSet<>();
@@ -95,13 +95,13 @@ public class WeakReferenceTests {
         threads.clear();
         Runtime.getRuntime().gc();
 
-        for (finalizableTest obj: weakHashMap.values()) {
+        for (FinalizableTest obj: weakHashMap.values()) {
             System.out.println("WEAKHASHAMP THREAD value found "+obj);
         }
         System.out.println("SIZE OF WEAK MAP THREAD "+ weakHashMap.size());
         System.out.println("SIZE OF STRONG MAP THREAD "+ strongMap.size());
 
-        for (finalizableTest obj: strongMap.values()) {
+        for (FinalizableTest obj: strongMap.values()) {
             System.out.println("STRONG MAP value found THREAD "+obj.toString());
         }
 
@@ -113,8 +113,8 @@ public class WeakReferenceTests {
         }
     }
 
-    private static Thread createThreadWithFinalizeTest(Map<Thread, finalizableTest> testMap) {
-        finalizableTest value1 = new finalizableTest(testMap);
+    private static Thread createThreadWithFinalizeTest(Map<Thread, FinalizableTest> testMap) {
+        FinalizableTest value1 = new FinalizableTest(testMap);
         Thread key = new Thread(() -> System.out.println("RUNNING THREAD " + Thread.currentThread().getName() + "  value " + value1.toString()));
         testMap.put(key, value1);
         return key;
