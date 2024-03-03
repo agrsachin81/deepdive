22d981b865717a48d9e92fbb5b9d052e7c6bb392
agrsachin81 <agrsachin81@gmail.com>
2024-03-03 22:31:53 +0530

    updated comments

--- src/main/java/com/champsworld/concurrent/OrderedTask.java
+++ src/main/java/com/champsworld/concurrent/OrderedTask.java
@@ -3,9 +3,11 @@ package com.champsworld.concurrent;
 import java.util.function.Supplier;
 
 /**
- * get is called asynchronously with in ordering id bound
+ * The Ordered Task with having same orderingId/concurrencyId will be executed in order of submitting.
+ * Ideally the orderingId should be object System hashcode as it is the unique for each object
+ * @param <V>
  * @author agrsachin81
  */
 public interface OrderedTask<V> extends OrderedTaskSpec<V>, Supplier<V> {
-
+    
 }
